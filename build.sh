00d3c0b9d6e116d66f4a2facea640d20b3ce9d3d
Ben Guler <benguler827@gmail.com>
2022-12-18 05:01:25 -0500

    commit 6

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -o errexit
 poetry install
 poetry update
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
