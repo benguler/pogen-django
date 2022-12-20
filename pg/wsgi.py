0efc794c5c72c9c618f4da70f000374deba51fd6
Ben Guler <benguler827@gmail.com>
2022-12-20 03:17:02 -0500

    commit 3

--- pg/wsgi.py
+++ pg/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/bguler/pogen'
+
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pg.settings')
 
 application = get_wsgi_application()
